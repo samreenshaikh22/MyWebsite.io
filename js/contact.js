8077a0250283ff0d85355fedf4d1757535f79c7d
samreenshaikh22 <97801281+samreenshaikh22@users.noreply.github.com>
2022-01-16 12:08:10 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -2,6 +2,7 @@ function sendMail(params){
   var tempParams={
     from_name:document.getElementById("from_name").value,
     message:document.getElementById("message").value, 
+    email:document.getElementById("email").value, 
   };
   emailjs.send("service_xstypie","template_77m9r45",tempParams)
   .then(function(res){
