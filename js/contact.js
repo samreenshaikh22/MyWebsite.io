01c8995649fc555aab2dc932cbcd948a976f44d5
samreenshaikh22 <97801281+samreenshaikh22@users.noreply.github.com>
2022-01-16 12:00:56 +0530

    Create contact.js

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+function sendMail(params){
+  var tempParams={
+    from_name:document.getElementById("from_name").value,
+    message:document.getElementById("message").value, 
+  };
+  emailjs.send("service_xstypie","template_77m9r45",tempParams)
+  .then(function(res){
+    console.log("success", res.status);
+  })
+}
+
